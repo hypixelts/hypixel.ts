a7c0b62ec8d52a395081edd374e14afba9999e0b
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-28 18:32:44 +0100

    fix: add error messages

--- src/errors/Messages.ts
+++ src/errors/Messages.ts
@@ -4,7 +4,9 @@ const messages = {
 	CLIENT_OPTIONS_MISSING: (option: string) => `ClientOptions.${option} is missing`,
 	CLIENT_OPTION_INVALID_TYPE: (option: string, type: string) => `ClientOption.${option} is expected to be of type "${type}"`,
 	GET_UUID_ERROR: (error: string, code: number) => `Util.getUUID: Failed with error ${error}, status code ${code}`,
-	GET_UUID_404: `Util.getUUID: Player not found`
+	GET_UUID_404: `Util.getUUID: Player not found`,
+	GET_USERNAME_ERROR: (error: string, code: number) => `Util.getUsername: Failed with error ${error}, status code ${code}`,
+	GET_USERNAME_404: `Util.getUsername: Player not found`
 };
 
 for (const [key, value] of Object.entries(messages)) register(key, value);
