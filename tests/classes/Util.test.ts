abe82445f62900749637186d869042987636e19b
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-29 12:17:39 +0100

    fix: isUUID test wasn't using function

--- tests/classes/Util.test.ts
+++ tests/classes/Util.test.ts
@@ -1,5 +1,6 @@
 import { Client } from '../../src';
 import { test, expect } from 'vitest';
+import { v4 } from 'uuid';
 
 const client = new Client(process.env.HYPIXEL_API_KEY!);
 
@@ -9,9 +10,9 @@ test('Util.getUUID to be correct UUID', async () => {
 });
 
 test('Util.isUUID matches UUID Regex', () => {
-	const uuid = 'ab589c4ed6804cd1b5ff3259980fb633';
-	const regex = /^[0-9a-f]{32}$/i;
-	expect(uuid).toMatch(regex);
+	const uuid = v4();
+
+	expect(client.util.isUUID(uuid)).toBeTruthy();
 });
 
 test('Util.getUsername to be the correct Username', async () => {
